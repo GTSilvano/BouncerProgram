ea1402a85f33e45155eb3f459acf31c00fb006f6
GTSilvano <silvano.condrea@gmail.com>
2022-08-08 16:19:37 +0300

    first bouncer task

---
+++ bouncer.py
@@ -0,0 +1,13 @@
+print("Welcome to our club.")
+
+age = int(input("What's your age?"))
+
+if age >= 18:
+    print("Please enter. Enjoy your evening!")
+else:
+    print("Get the fuck out you underage dog!")
+
+def main():
+      
+    if __name__ == '__main__':
+         main()
