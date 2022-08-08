e4875f3d4de19894786a2d1816cf18612a411eb1
GTSilvano <silvano.condrea@gmail.com>
2022-08-08 22:11:03 +0300

    minor correction

--- bouncer.py
+++ bouncer.py
@@ -1,28 +1,29 @@
-print("Welcome to our club.")
+def main():
+    print("Welcome to our club.")
 
-age = int(input("What's your age? "))
+    age = int(input("What's your age? "))
 
 
-if age >=21: 
-   print("Come on in.")
-else:
-    country = str(input("Where are you from? "))
-    if country == "USA" and age <=21:
-        print("Get out")
+    if age >=21: 
+        print("Come on in.")
     else:
-        if country == "Mali" and age >=15:
-            print("Come on in.")
+        country = str(input("Where are you from? "))
+        if country == "USA" and age <=21:
+            print("Get out")
         else:
-            if age >=18:
+            if country == "Mali" and age >=15:
                 print("Come on in.")
-            else: 
-                print("Get out.")
-
+            else:
+                if age >=18:
+                    print("Come on in.")
+                else: 
+                    print("Get out.")
 
 
 
 
 
       
-    #if __name__ == '__main__':
-    #     main()
+if __name__ == '__main__':
+    main()
+    
