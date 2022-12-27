8ccc71025986a98d32de2307805ba0d694e76a71
GTSilvano <silvano.condrea@gmail.com>
2022-12-27 20:57:21 +0200

    Updated the code with a dictionary to optimize the code

--- bouncer.py
+++ bouncer.py
@@ -1,29 +1,30 @@
 def main():
     print("Welcome to our club.")
 
+    age_restrictions = {
+        "USA": 21,
+        "Mali": 15,
+        "Moldova": 18,
+        "Germany": 16,
+        "Iceland": 20
+    }
+
     age = int(input("What's your age? "))
 
-
-    if age >=21: 
+    if age >= 21:
         print("Come on in.")
     else:
         country = str(input("Where are you from? "))
-        if country == "USA" and age <=21:
-            print("Get out")
-        else:
-            if country == "Mali" and age >=15:
+        if country in age_restrictions:
+            if age >= age_restrictions[country]:
                 print("Come on in.")
             else:
-                if age >=18:
-                    print("Come on in.")
-                else: 
-                    print("Get out.")
+                print("Get out.")
+        elif age >= 18:
+            print("Come on in.")
+        else:
+            print("Get out.")
 
-
-
-
-
-      
 if __name__ == '__main__':
     main()
     
