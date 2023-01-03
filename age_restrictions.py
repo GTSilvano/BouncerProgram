eba2aaeef97f4d7c149655d5dba33aed1f1f1db8
GTSilvano <silvano.condrea@gmail.com>
2023-01-03 13:32:00 +0200

    trying to use a new library and saving content.

--- age_restrictions.py
+++
@@ -1,7 +0,0 @@
-age_restrictions = {
-        "USA": 21,
-        "Mali": 15,
-        "Moldova": 18,
-        "Germany": 16,
-        "Iceland": 20
-    }
--- bouncer.py
+++ bouncer.py
@@ -1,8 +1,13 @@
+import ast
+
+
+with open('someData.txt', 'r') as f:
+    contents = f.read()
+    contents = contents[contents.index('{'):]
+    age_restrictions = ast.literal_eval(contents)
+        
 def main():
     print("Welcome to our club.")
-
-    with open("age_restrictions.py") as f:
-        exec(f.read())
 
     age = int(input("What's your age? "))
 
@@ -10,8 +15,8 @@ def main():
         print("Come on in.")
     else:
         country = str(input("Where are you from? "))
-        if country in globals()["age_restrictions"]:
-            if age >= globals()["age_restrictions"][country]:
+        if country in age_restrictions:
+            if age >= age_restrictions[country]:
                 print("Come on in.")
             else:
                 print("Get out.")
